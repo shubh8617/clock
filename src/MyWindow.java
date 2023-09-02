b70c42c44886a7207dbdb54f03c0b04fd1223047
Shubh <ssonthalia0123@gmail.com>
2023-09-02 13:24:31 +0530

    Building Clock using Java

---
+++ src/MyWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MyWindow extends JFrame {
+
+    MyWindow()
+    {
+        super.setTitle("My Clock");
+        super.setSize(500, 500);
+        super.setLocation(300,50);
+
+        super.setVisible(true);
+    }
+}
